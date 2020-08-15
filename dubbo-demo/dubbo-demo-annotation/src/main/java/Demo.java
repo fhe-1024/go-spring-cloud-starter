c0764126dd7186297483346d5e471f37cffbe58f
hefei <hefei@bizvane.cn>
2020-08-16 00:19:46 +0800

    init

--- dubbo-demo/dubbo-demo-annotation/src/main/java/Demo.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-public class Demo {
-
-    public static void main(String[] args) {
-        AnnotationConfigApplicationContext annotationConfigApplicationContex = new AnnotationConfigApplicationContext();
-        annotationConfigApplicationContex.refresh();
-    }
-}
