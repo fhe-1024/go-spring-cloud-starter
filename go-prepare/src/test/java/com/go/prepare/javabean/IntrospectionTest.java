8259c1eefdbb5f1f018f303f75f446ae8897b322
hefei@bizvane.com <hefei@bizvane.com>
2020-08-17 10:17:47 +0800

    java自省

---
+++ go-prepare/src/test/java/com/go/prepare/javabean/IntrospectionTest.java
@@ -0,0 +1,33 @@
+package com.go.prepare.javabean;
+
+import org.junit.Test;
+
+import java.beans.BeanInfo;
+import java.beans.IntrospectionException;
+import java.beans.Introspector;
+import java.beans.PropertyEditorSupport;
+import java.util.stream.Stream;
+
+public class IntrospectionTest {
+
+    @Test
+    public void personIntrospectionTest() throws IntrospectionException {
+        BeanInfo beanInfo = Introspector.getBeanInfo(Person.class, Object.class);
+        Stream.of(beanInfo.getPropertyDescriptors())
+                .forEach(propertyDescriptor -> {
+                    Class<?> propertyClass = propertyDescriptor.getPropertyType();
+                    if (propertyClass == Integer.class) {
+                        propertyDescriptor.setPropertyEditorClass(StringToIntegralPropertyEditor.class);
+                    }
+                });
+    }
+
+
+    static class StringToIntegralPropertyEditor extends PropertyEditorSupport {
+        @Override
+        public void setAsText(String text) throws IllegalArgumentException {
+            setValue(Integer.valueOf(text));
+        }
+    }
+
+}
